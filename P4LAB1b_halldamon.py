71a873ec051f6ff7032f5c415682a3888d7ad65b
damonSAhall <145627594+damonSAhall@users.noreply.github.com>
2023-11-09 16:52:20 -0500

    Add files via upload

---
+++ P4LAB1b_halldamon.py
@@ -0,0 +1,20 @@
+
+import turtle
+t = turtle.Turtle()
+for x in range(4):
+  t.circle(50, 360/8)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(180)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(180)
+t.forward(100)
+
